99fefbaa952dcb52833a5121fd52b5768718e298
SIDDHARTH DEORE <siddharthdeore@gmail.com>
2017-04-12 17:29:57 +0530

    Update grashof.m
    error corected

--- grashof.m
+++ grashof.m
@@ -3,8 +3,8 @@ function output = grashof(N)
     while 1             %infinete loop
         A=rand(1,4)*5;  % generate row of 4 random numbers from 0 to 5
         sA=sort(A);     % sort the row
-        SL=A(1)+A(4);   % sumation of shortest and largest element in row
-        PQ=A(2)+A(3);   % sumation of remaining element in row
+        SL=sA(1)+sA(4);   % sumation of shortest and largest element in row
+        PQ=sA(2)+sA(3);   % sumation of remaining element in row
         if(SL<=PQ)      % check grashof's law
             if(exist('output', 'var')) % check if output row exist append new set to output
                 output=[output; A];
